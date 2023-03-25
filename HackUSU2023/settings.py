416a99af28d7c42b873463d4d06f4a837486f653
Treyson-Grange <treyson.grange@outlook.com>
2023-03-24 19:42:17 -0600

    working server

--- HackUSU2023/settings.py
+++ HackUSU2023/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'CYOA.apps.CyoaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
