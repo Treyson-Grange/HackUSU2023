38f8f33a41dffe8cc10af84fa9cc222b284656a5
Treyson-Grange <treyson.grange@outlook.com>
2023-03-24 21:30:39 -0600

    asdflk

--- CYOA/static/CYOA/script.js
+++ CYOA/static/CYOA/script.js
@@ -1,9 +1,6 @@
-console.log("hello")
-console.log("")
-console.log("")
-
 class Node {
-    constructor(text, imageURL) {
+    constructor(text, imageURL, decision) {
+        this.decision = decision;
         this.imageURL = imageURL;
         this.text = text;
         this.left = null;
@@ -43,41 +40,66 @@ class BinaryTree {
             }
         }
     }
+    update() {
+        let firstOption = document.getElementById("first");
+        firstOption.textContent = this.currentNode.left.decision;
+        let secondOption = document.getElementById("second");
+        secondOption.textContent = this.currentNode.right.decision;
+    }
     goLeft() {
         this.currentNode = this.currentNode.left;
+        this.update();
     }
     goRight() {
         this.currentNode = this.currentNode.right;
+        this.update();
     }
+    
 }
-const test = new BinaryTree();
-test.insert("1", "1");
-console.log(test.root.text);
-test.insert("2","2");
-test.insert("3","3");
-test.insert("3","3");
-test.insert("3","3");
-test.insert("3","3");
-test.insert("4","4");
-test.insert("6","6");
-test.insert("7","8");
-test.insert("2","9");
-// console.log(test.root.left.left);
 
 
-
-//gets the h1 tag
 let firstOption = document.getElementById("first");
 firstOption.textContent = "test";
 //gets the h1 tag
 let secondOption = document.getElementById("second");
 
+const test = new BinaryTree();
+
+
+
+test.root = new Node("root","t","d");
+test.currentNode = test.root;
+test.root.left = new Node("left","t","d");
+test.root.right = new Node("right","t","dd");
+test.root.left.left = new Node("leftleft","t","ddd");
+test.root.left.right = new Node("leftright","t","dddd");
+test.root.right.left = new Node("rightleft","t","ddddd");
+test.root.right.right = new Node("rightright","t","dddddd");
+test.root.left.left.left = new Node("leftleftleft","t","ddddddd");
+test.root.left.left.right = new Node("leftleftright","t","dddddddd");
+test.root.left.right.left = new Node("leftrightleft","t","ddddddddd");
+test.root.left.right.right = new Node("leftrightright","t","dddddddddd");
+test.root.right.left.left = new Node("rightleftleft","t","ddddddddddd");
+test.root.right.left.right = new Node("rightleftright","t","ddddddddddddd");
+test.root.right.right.left = new Node("rightrightleft","t","dddddddddddddd");
+test.root.right.right.right = new Node("rightrightright","t","dddddddddddddd");
+
+firstOption.textContent = test.currentNode.left.decision;
+secondOption.textContent = test.currentNode.right.decision;
+
+
+console.log(test.currentNode.text);
+
+//gets the h1 tag
+
+
 firstOption.onclick = function() {
     console.log("clicked");
     //Call a function that goes to the left of the current node
-    
+    test.goLeft();
 }
 secondOption.onclick = function() {
     console.log("clicked");
     //Call a function that goes to the right of the current node
+    test.goRight();
 }
